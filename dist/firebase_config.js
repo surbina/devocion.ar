cf6391593bff930c79c4e59c09a5f034a6f86633
Sebastian Urbina <sebita.urbina@gmail.com>
2016-06-25 10:13:19 -0300

    Creating firebase config script

---
+++ dist/firebase_config.js
@@ -0,0 +1,7 @@
+var config = {
+  apiKey: "<YOUR-API-KEY>",
+  //authDomain: "<YOUR-AUTH-DOMAIN>",
+  databaseURL: "<YOUR-DB-URL>",
+  storageBucket: "<YOUR-STORAGE-BUCKET>",
+};
+firebase.initializeApp(config);
