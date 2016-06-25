5a1dce5e6bb48d5dc33c4c9cd0131fbf50ae1ee8
Sebastian Urbina <sebita.urbina@gmail.com>
2016-06-25 10:34:30 -0300

    fixed untracked files

--- dist/firebase_config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-  apiKey: "<YOUR-API-KEY>",
-  //authDomain: "<YOUR-AUTH-DOMAIN>",
-  databaseURL: "<YOUR-DB-URL>",
-  storageBucket: "<YOUR-STORAGE-BUCKET>",
-};
-firebase.initializeApp(config);
