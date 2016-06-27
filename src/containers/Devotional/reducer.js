a69afa736ab99f4157a5c841d4bddce6ec111231
Sebastian Urbina <sebastian.urbina@globant.com>
2016-06-27 14:42:35 -0300

    Refactor reducer functions

--- src/containers/Devotional/reducer.js
+++ src/containers/Devotional/reducer.js
@@ -2,15 +2,13 @@ import { Map } from 'immutable';
 import { REQUEST_DEVOTIONAL, REQUEST_DEVOTIONAL_SUCCESS } from './actions.js';
 
 function requestDevotional(state, id) {
-  const devotional_list = state.get('devotional_list').merge({
-    [id]: {
-      fetching: true,
-      valid: false
-    }
-  });
-
   return state.merge({
-    devotional_list: devotional_list,
+    devotional_list: {
+      [id]: {
+        fetching: true,
+        valid: false
+      }
+    },
     home_section: {
       current_devotional: id.toString()
     }
@@ -18,20 +16,20 @@ function requestDevotional(state, id) {
 }
 
 function requestDevotionalSuccess(state, devotional) {
-  const devotional_list = state.get('devotional_list').merge({
-    [devotional.id]: {
-      fetching: false,
-      valid: true,
-      id: devotional.id,
-      title: devotional.title,
-      pasagge: devotional.passagge,
-      body: devotional.body,
-      author: devotional.author,
-      publishDate: devotional.publishDate,
+  return state.merge({
+    devotional_list: {
+      [devotional.id]: {
+        fetching: false,
+        valid: true,
+        id: devotional.id,
+        title: devotional.title,
+        pasagge: devotional.passagge,
+        body: devotional.body,
+        author: devotional.author,
+        publishDate: devotional.publishDate,
+      }
     }
   });
-
-  return state.merge({devotional_list: devotional_list});
 }
 
 export default function(state = Map({devotional_list: {}}), action) {
