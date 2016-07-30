2332eaded9f1b887b36735ec9f91b57a48a39bd2
Sebastian Urbina <Sebastian Urbina>
2016-07-30 17:29:42 -0300

    Adding firebase config file

---
+++ dist/firebase_config.js
@@ -0,0 +1,7 @@
+var config = {
+  apiKey: "<YOUR-API-KEY>",
+  //authDomain: "<AUTH-DOMAIN>",
+  databaseURL: "<DATABASE-URL>",
+  storageBucket: "<STORAGE-BUCKET>",
+};
+firebase.initializeApp(config);
