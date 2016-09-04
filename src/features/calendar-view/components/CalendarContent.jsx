39278d688937e5ca80d7a8dcb909936816e800a8
Sebastian Urbina <sebita.urbina@gmail.com>
2016-09-04 14:01:02 -0300

    Added Calendar View

--- src/features/calendar-view/components/CalendarContent.jsx
+++ src/features/calendar-view/components/CalendarContent.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-
-import CalendarContent from '../components/CalendarContent.jsx';
+import InfiniteCalendar from 'react-infinite-calendar';
+import moment from 'moment';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
+  locale: {
+    name: 'es',
+    headerFormat: 'dddd, Do MMM',
+    months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+    weekdays: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
+    weekdaysShort: ['dom.', 'lun.', 'mar.', 'mié.', 'jue.', 'vie.', 'sáb.'],
+    blank: 'Seleccione una fecha ...',
+    todayLabel: {
+     long: 'Hoy',
+     short: 'Hoy'
+    }
+  },
   render: function() {
     return(
-      <CalendarContent />
+      <InfiniteCalendar
+        width={'100%'}
+        height={window.innerHeight - 175}
+        rowHeight={100}
+        selectedDate={moment()}
+        keyboardSupport={true}
+        showHeader={false}
+        locale={this.locale}
+        onSelect={this.props.onSelect}
+      />
     );
   }
 });
--- src/features/calendar-view/containers/Calendar.jsx
+++ src/features/calendar-view/containers/Calendar.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { connect } from 'react-redux';
-import InfiniteCalendar from 'react-infinite-calendar';
+import { hashHistory  } from 'react-router';
+
+import CalendarContent from '../components/CalendarContent.jsx';
 
 export const Calendar = React.createClass({
   mixins: [PureRenderMixin],
+  handleSelectedDate: function(date) {
+    hashHistory.push('/devotional/' + date.format('YYYY-MM-DD'));
+  },
   render: function() {
-    // Render the Calendar
-    const today = new Date();
-    const minDate = Number(new Date()) - (24*60*60*1000) * 7; // One week before today
-
     return(
-      <InfiniteCalendar
-        width={400}
-        height={600}
-        selectedDate={today}
-        disabledDays={[0,6]}
-        minDate={minDate}
-        keyboardSupport={true}
-      />
+      <div className="row">
+        <div className="col-xs-12 text-center">
+          <h3>Calendario</h3>
+        </div>
+        <div className="col-xs-12">
+          <CalendarContent onSelect={this.handleSelectedDate} />
+        </div>
+      </div>
     );
   }
 });
