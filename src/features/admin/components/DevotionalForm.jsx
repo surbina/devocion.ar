ce9d6a4dc937a2361a33b0f23a6d83c1f0769ae5
Sebastian Urbina <sebita.urbina@gmail.com>
2016-09-06 15:45:05 -0300

    Added validation to Devotional form and Comment form

--- src/features/admin/components/DevotionalForm.jsx
+++ src/features/admin/components/DevotionalForm.jsx
@@ -6,6 +6,7 @@ import Datetime  from 'react-datetime';
 import moment from 'moment';
 import { Map } from 'immutable';
 import { ThreeBounce } from 'better-react-spinkit';
+import classNames from 'classnames';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
@@ -18,22 +19,78 @@ export default React.createClass({
   getInitialState: function() {
     return {
       title: this.props.model.title,
+      titleValid: false,
+      titleValidationMessage: '',
       passage: this.props.model.passage,
+      passageValid: false,
+      passageValidationMessage: '',
       body: this.props.model.body,
-      publish_date: this.props.model.publish_date ? moment(this.props.model.publish_date) : ''
+      bodyValid: false,
+      bodyValidationMessage: '',
+      publish_date: this.props.model.publish_date ? moment(this.props.model.publish_date) : '',
+      publish_dateValid: false,
+      publish_dateValidationMessage: ''
     };
   },
   handleTitleChange: function(e) {
     this.setState({title: e.target.value});
   },
+  validateTitle: function() {
+    const titleValue = this.state.title.trim();
+    const isValid = !!titleValue && titleValue.length < 50;
+    const validationMessage = !isValid ? 'Por favor completa el título, la longitud del mismo debe ser menor a 50 caracteres' : '';
+
+    this.setState({
+      titleValid: isValid,
+      titleValidationMessage: validationMessage
+    });
+
+    return isValid;
+  },
   handlePassageChange: function(e) {
     this.setState({passage: e.target.value});
+  },
+  validatePassage: function() {
+    const passageValue = this.state.passage.trim();
+    const isValid = !!passageValue && passageValue.length < 20;
+    const validationMessage = !isValid ? 'Por favor completa el pasaje, la longitud del mismo debe ser menor a 20 caracteres' : '';
+
+    this.setState({
+      passageValid: isValid,
+      passageValidationMessage: validationMessage
+    });
+
+    return isValid;
   },
   handleBodyChange: function(e) {
     this.setState({body: e.target.value});
   },
+  validateBody: function() {
+    const bodyValue = this.state.body.trim();
+    const isValid = !!bodyValue && bodyValue.length < 1500;
+    const validationMessage = !isValid ? 'Por favor completa el contenido del devocional, la longitud del mismo debe ser menor a 1500 caracteres' : '';
+
+    this.setState({
+      bodyValid: isValid,
+      bodyValidationMessage: validationMessage
+    });
+
+    return isValid;
+  },
   handlePublishDateChange: function(e) {
-    this.setState({publish_date: e});
+    this.setState({publish_date: e}, this.validatePublishDate);
+  },
+  validatePublishDate: function() {
+    const publishDateValue = this.state.publish_date;
+    const isValid = !!publishDateValue;
+    const validationMessage = !isValid ? 'Por favor completa la fecha del devocional' : '';
+
+    this.setState({
+      publish_dateValid: isValid,
+      publish_dateValidationMessage: validationMessage
+    });
+
+    return isValid;
   },
   handleSubmit: function(e) {
     e.preventDefault();
@@ -41,7 +98,10 @@ export default React.createClass({
     const passage = this.state.passage.trim();
     const publish_date = this.state.publish_date;
     const body = this.state.body.trim();
-    if (!title || !passage || !publish_date || !body) {
+    if (!this.validateTitle() |
+        !this.validatePassage() |
+        !this.validatePublishDate() |
+        !this.validateBody()) {
       return;
     }
 
@@ -63,10 +123,37 @@ export default React.createClass({
     });
   },
   render: function() {
+    const titleClass = classNames({
+      'col-md-5': true,
+      'col-xs-12': true,
+      'multiple-row': true,
+      'has-error': !!this.state.titleValidationMessage
+    });
+
+    const passageClass = classNames({
+      'col-md-5': true,
+      'col-xs-12': true,
+      'multiple-row': true,
+      'has-error': !!this.state.passageValidationMessage
+    });
+
+    const publishDateClass = classNames({
+      'col-md-2': true,
+      'col-xs-12': true,
+      'multiple-row': true,
+      'has-error': !!this.state.publish_dateValidationMessage
+    });
+
+    const bodyClass = classNames({
+      'col-md-12': true,
+      'col-xs-12': true,
+      'has-error': !!this.state.bodyValidationMessage
+    });
+
     return(
       <form className="form-horizontal devotional-form" onSubmit={this.handleSubmit}>
         <div className="form-group">
-          <div className="col-md-5 col-xs-12 multiple-row">
+          <div className={ titleClass }>
             <input
               type="text"
               placeholder="Título"
@@ -74,10 +161,14 @@ export default React.createClass({
               id="inputTitle"
               value={this.state.title}
               onChange={this.handleTitleChange}
+              onBlur={this.validateTitle}
             />
+            {!!this.state.titleValidationMessage ?
+              <span id="titleHelpBlock" className="help-block">{this.state.titleValidationMessage}</span> :
+              false}
           </div>
 
-          <div className="col-md-5 col-xs-12 multiple-row">
+          <div className={ passageClass }>
             <input
               type="text"
               placeholder="Pasaje"
@@ -85,22 +176,31 @@ export default React.createClass({
               id="inputPassage"
               value={this.state.passage}
               onChange={this.handlePassageChange}
+              onBlur={this.validatePassage}
             />
+            {!!this.state.passageValidationMessage ?
+              <span id="passageHelpBlock" className="help-block">{this.state.passageValidationMessage}</span> :
+              false}
           </div>
 
-          <div className="col-md-2 col-xs-12 multiple-row">
+          <div className={ publishDateClass }>
             <Datetime
               value={this.state.publish_date}
               onChange={this.handlePublishDateChange}
               dateFormat="DD/MM/YYYY"
               timeFormat={false}
               closeOnSelect={true}
-              inputProps={{placeholder: 'Fecha de publicación'}}/>
+              inputProps={{placeholder: 'Fecha de publicación'}}
+              onBlur={this.validatePublishDate}
+            />
+            {!!this.state.publish_dateValidationMessage ?
+              <span id="publishDateHelpBlock" className="help-block">{this.state.publish_dateValidationMessage}</span> :
+              false}
           </div>
         </div>
 
         <div className="form-group">
-          <div className="col-md-12 col-xs-12">
+          <div className={ bodyClass }>
             <textarea
               id="textBody"
               placeholder="Contenido ..."
@@ -108,8 +208,12 @@ export default React.createClass({
               rows="10"
               value={this.state.body}
               onChange={this.handleBodyChange}
+              onBlur={this.validateBody}
             >
             </textarea>
+            {!!this.state.bodyValidationMessage ?
+              <span id="bodyDateHelpBlock" className="help-block">{this.state.bodyValidationMessage}</span> :
+              false}
           </div>
         </div>
         <div className="form-group">
--- src/features/devotional-view/components/CommentForm.jsx
+++ src/features/devotional-view/components/CommentForm.jsx
@@ -5,6 +5,7 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import moment from 'moment';
 import { Map } from 'immutable';
 import { ThreeBounce } from 'better-react-spinkit';
+import classNames from 'classnames';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
@@ -15,15 +16,31 @@ export default React.createClass({
     isSubmitting: React.PropTypes.bool.isRequired
   },
   getInitialState: function() {
-    return {comment_body: ''};
+    return {
+      comment_body: '',
+      comment_bodyValid: false,
+      comment_bodyValidationMessage: ''
+    };
   },
   handleCommentBodyChange: function(e) {
     this.setState({comment_body: e.target.value});
   },
+  validateCommentBody: function() {
+    const commentValue = this.state.comment_body.trim();
+    const isValid = !!commentValue && commentValue.length <= 500;
+    const validationMessage = !isValid ? 'Los comentarios tienen un maximo de 500 caracteres' : '';
+
+    this.setState({
+      comment_bodyValid: isValid,
+      comment_bodyValidationMessage: validationMessage
+    });
+
+    return isValid;
+  },
   handleSubmit: function(e) {
     e.preventDefault();
     const comment_body = this.state.comment_body.trim();
-    if(!comment_body) {
+    if(!this.validateCommentBody()) {
       return;
     }
 
@@ -38,9 +55,14 @@ export default React.createClass({
     this.setState({comment_body: ''});
   },
   render: function() {
+    const commentBodyClasses = classNames({
+      'form-group': true,
+      'has-error': !!this.state.comment_bodyValidationMessage
+    });
+
     return(
       <form className="form-horizontal comment-form" onSubmit={this.handleSubmit}>
-        <div className="form-group">
+        <div className={ commentBodyClasses }>
           <div className="col-sm-12">
             <textarea
               className="form-control vresize"
@@ -51,6 +73,9 @@ export default React.createClass({
               onChange={this.handleCommentBodyChange}
             >
             </textarea>
+            {!!this.state.comment_bodyValidationMessage ?
+              <span id="commentBodyHelpBlock" className="help-block">{this.state.comment_bodyValidationMessage}</span> :
+              false}
           </div>
         </div>
         <div className="form-group">
